3b9a7011a541f53f6e8937038019f32bf2a765c1
Tim MacNeil <tim@timmacneil.com>
2021-01-15 20:53:22 -0400

    Implemented dice display in the canvas.

--- client/templates/dice.js
+++ client/templates/dice.js
@@ -4,11 +4,16 @@ function DiceGame()
 {
 	var that = this;
 
+	var DICE_MAX_VALUE = 6;
+	var DICE_MIN_VALUE = 1;
+
 	this.websocket = undefined;
 	this.name = "";
 	this.room_code = "";
 	this.last_message = "";
+	this.in_room = false;
 
+	this.is_game_active = false;
 	this.can_start = false;
 	this.can_roll = false;
 	this.can_stop = false;
@@ -25,12 +30,37 @@ function DiceGame()
 	this.controls.roll_btn.disabled = true;
 	this.controls.stop_roll_btn.disabled = true;
 	this.display = {
+		sign_in: document.getElementById("signin"),
+		game_room: document.getElementById("game_room"),
+		player_name: document.getElementById("player_name"),
+		room_code: document.getElementById("room_code"),
 		member_list: document.getElementById("member_list"),
+		game_space: document.getElementById("game_space"),
+		current_turn_name: document.getElementById("current_turn_name"),
+		current_score: document.getElementById("current_score"),
+		last_roll_score: document.getElementById("last_roll_score"),
+		dice_canvas: document.getElementById("dice"),
+		debug_console: document.getElementById("debug_console"),
 	};
 
+	this.dice_ctx = this.display.dice_canvas.getContext("2d");
+	this.offscreen_canvas = document.createElement("canvas");
+	this.offscreen_canvas.width = that.display.dice_canvas.width;
+	this.offscreen_canvas.height = that.display.dice_canvas.height;
+	this.offscreen_ctx = this.offscreen_canvas.getContext("2d");
+	this.tilesheet_loaded = false;
 	this.tilesheet_img = new Image();
+	this.tilesheet_img.onload = function()
+	{
+		that.tilesheet_loaded = true;
+	}
+	this.tilesheet_img.src = TOP_URL + "/dice-tilesheet.png";
 	this.tile_width = 64;
 	this.tile_height = 64;
+	this.tile_padding = 8;
+
+	// TODO: Customisable
+	this.dice_set = 1;
 
 	if ("WebSocket" in window)
 	{
@@ -50,6 +80,49 @@ function DiceGame()
 			// TODO: Cleanup?
 			// Switch back to join screen
 		}
+	}
+
+	this.flip_canvas = function()
+	{
+		that.dice_ctx.clearRect(0, 0,
+			that.display.dice_canvas.width, that.display.dice_canvas.height);
+		that.dice_ctx.drawImage(that.offscreen_canvas, 0, 0);
+	}
+
+	this.draw_die = function(position, value, locked)
+	{
+		lock_x = DICE_MAX_VALUE * that.tile_width;
+		if (that.tilesheet_loaded && that.is_game_active)
+		{
+			src_x = (value - 1) * that.tile_width;
+			src_y = that.dice_set * that.tile_height;
+			dst_x = (position + 1) * that.tile_padding
+				+ (position * that.tile_width);
+			dst_y = that.tile_padding;
+			that.offscreen_ctx.drawImage(that.tilesheet_img,
+				src_x, src_y, that.tile_width, that.tile_height,
+				dst_x, dst_y, that.tile_width, that.tile_height);
+
+			if (locked)
+			{
+				that.offscreen_ctx.drawImage(that.tilesheet_img,
+					lock_x, src_y, that.tile_width, that.tile_height,
+					dst_x, dst_y, that.tile_width, that.tile_height);
+			}
+		}
+	}
+
+	this.draw_dice = function()
+	{
+		that.offscreen_ctx.clearRect(0, 0,
+			that.display.dice_canvas.width, that.display.dice_canvas.height);
+
+		dice = that.game_state.dice;
+		for (var i = 0; i < dice.length; i++)
+		{
+			that.draw_die(i, dice[i].value, dice[i].locked);
+		}
+		that.flip_canvas();
 	}
 
 	this.update_game_state = function(new_state)
@@ -76,35 +149,63 @@ function DiceGame()
 
 	this.update_display = function()
 	{
-		// Switch to game display
-		var sign_in = document.getElementById("signin");
-		sign_in.style = "display: none;"
-		var game_room = document.getElementById("game_room");
-		game_room.style = "display: initial;"
-
-		// Button states
-		that.controls.start_btn.disabled = !that.can_start;
-		that.controls.roll_btn.disabled = !that.can_roll;
-		that.controls.stop_roll_btn.disabled = !that.can_stop;
-
-		// Member list
-		while (that.display.member_list.lastChild)
+		if (that.in_room)
 		{
-			that.display.member_list.removeChild(
-				that.display.member_list.lastChild
-			);
-		}
-		for (var i = 0; i < that.room_members.length; i++)
+			// Switch to game_room display
+			that.display.sign_in.style.display = "none";
+			that.display.game_room.style.display = "initial";
+
+			// Room info
+			that.display.player_name.innerHTML = that.name;
+			that.display.room_code.innerHTML = that.room_code;
+
+			// Button states
+			that.controls.start_btn.disabled = !that.can_start;
+			that.controls.roll_btn.disabled = !that.can_roll;
+			that.controls.stop_roll_btn.disabled = !that.can_stop;
+
+			if (that.is_game_active)
+			{
+				// Game info
+				that.display.current_turn_name.innerHTML
+					= that.game_state.current_turn_name;
+				that.display.current_score.innerHTML
+					= that.game_state.current_score;
+				that.display.last_roll_score.innerHTML
+					= that.game_state.last_roll_score;
+
+				// Dice
+				that.draw_dice();
+
+				// Players
+				//TODO
+			}
+
+			// TODO: Game Over display?
+
+			// Room member list
+			while (that.display.member_list.lastChild)
+			{
+				that.display.member_list.removeChild(
+					that.display.member_list.lastChild
+				);
+			}
+			for (var i = 0; i < that.room_members.length; i++)
+			{
+				var item = document.createElement("li");
+				item.appendChild(document.createTextNode(
+					that.room_members[i]
+				));
+				that.display.member_list.appendChild(item);
+			}
+		} else
 		{
-			var item = document.createElement("li");
-			item.appendChild(document.createTextNode(
-				that.room_members[i]
-			));
-			that.display.member_list.appendChild(item);
+			// Show sign in form(s)
+			that.display.sign_in.style.display = "initial";
+			that.display.game_room.style.display = "none";
 		}
 
-		var p = document.getElementById("debug_console");
-		p.textContent = that.last_message;
+		that.display.debug_console.textContent = that.last_message;
 	}
 
 	this.parse_message = function(message)
@@ -121,6 +222,12 @@ function DiceGame()
 			that.show_error_msg(data.msg);
 		} else
 		{
+			if ("room_code" in data)
+			{
+				that.room_code = data.room_code;
+				that.in_room = true;
+			}
+
 			// Update members list
 			if ("members" in data)
 			{
@@ -128,17 +235,18 @@ function DiceGame()
 			}
 
 			// Parse game state
-			if ("is_game_active" in data && data.is_game_active
+			that.is_game_active = ("is_game_active" in data && data.is_game_active
 				&& "current_game" in data && data.current_game != null)
+			if (that.is_game_active)
 			{
 				that.update_game_state(data.current_game);
 			} else
 			{
 				that.can_start = ("can_start" in data && data.can_start);
 			}
-
-			that.update_display();
 		}
+
+		that.update_display();
 	}
 
 	this.send = function(command)
@@ -193,6 +301,9 @@ function DiceGame()
 	this.controls.start_btn.onclick = this.start_game;
 	this.controls.roll_btn.onclick = this.roll;
 	this.controls.stop_roll_btn.onclick = this.stop_roll;
+
+	// Update display
+	this.update_display();
 }
 
 function init()
